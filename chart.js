d1ac69292abd78a5bb69892cb03776e44fa5e710
ian <20951604+iansecretario@users.noreply.github.com>
2024-02-11 04:11:40 +0800

    Create chart.js

---
+++ chart.js
@@ -0,0 +1,33 @@
+document.onload = function() {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'Monthly Sales',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            plugins: {
+                legend: {
+                    labels: {
+                        font: {
+                            size: 14,
+                            weight: 'bold',
+                        }
+                    }
+                }
+            }
+        }
+    });
+};
