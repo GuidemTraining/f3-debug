2df43cc815f85c019b0f51beacebed6389eac4e5
ian <20951604+iansecretario@users.noreply.github.com>
2024-02-11 04:27:51 +0800

    Create chart5.js

---
+++ chart5.js
@@ -0,0 +1,23 @@
+function loadChart() {
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    label: 'Sales',
+                    data: [10, 20, 30, 25, 35, 45, 40],
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
